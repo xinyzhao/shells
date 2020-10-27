41f921b0d24723f83111da3f4017b2179c856ea9
zhaoxin <zhaoxin@meishubao.com>
2020-10-27 17:04:15 +0800

    Update export.sh

--- Export/export.sh
+++ Export/export.sh
@@ -2,10 +2,32 @@
 
 PATH=$PATH:/usr/local/bin
 
-PROJECT="YOUR PROJECT NAME"
-SCHEME="YOUR SCHEME NAME"
+GIT_REMOTE_URL="YOUR GIT REMOTE URL"
+echo GIT_REMOTE_URL=${GIT_REMOTE_URL}
+GIT_BRANCH="YOUR GIT BRANCH"
+echo GIT_BRANCH=${GIT_BRANCH}
 
-WORKSPACE="$PWD/${PROJECT}.xcworkspace"
+IS_INSIDE_WORK_TREE=`git rev-parse --is-inside-work-tree`
+echo "Is inside work tree=${IS_INSIDE_WORK_TREE}"
+
+git config remote.origin.url "${GIT_REMOTE_URL}"
+git fetch --tags --force --progress
+GIT_COMMIT=`git rev-parse refs/remotes/origin/${GIT_BRANCH}^{commit}`
+GIT_COMMIT_AUTHOR=`git log --pretty=format:"%an" ${GIT_COMMIT} -1`
+GIT_COMMIT_MESSAGE=`git log --pretty=format:"%s" ${GIT_COMMIT} -1`
+GIT_COMMIT_TIME=`git log --pretty=format:"%ci" ${GIT_COMMIT} -1`
+echo "Commit: ${GIT_COMMIT}"
+echo "Commit message: ${GIT_COMMIT_MESSAGE}"
+echo "Commit by ${GIT_COMMIT_AUTHOR} at ${GIT_COMMIT_TIME}"
+git config core.sparsecheckout
+git checkout -f ${GIT_COMMIT}
+
+PROJECT_NAME="YOUR PROJECT NAME"
+echo PROJECT_NAME=${PROJECT_NAME}
+SCHEME_NAME="YOUR SCHEME NAME"
+echo SCHEME_NAME=${SCHEME_NAME}
+
+WORKSPACE="$PWD/${PROJECT_NAME}.xcworkspace"
 echo WORKSPACE=$WORKSPACE
 
 RELATIVE_PATH="Export"
@@ -17,33 +39,32 @@ DATE_PATH="${ARCHIVES_PATH}/${DATE}"
 echo DATE_PATH=$DATE_PATH
 
 OPTIONS_PATH="Options"
-OPTIONS_FILE="$PWD/${RELATIVE_PATH}/${OPTIONS_PATH}/${SCHEME}.plist"
+OPTIONS_FILE="$PWD/${RELATIVE_PATH}/${OPTIONS_PATH}/${SCHEME_NAME}.plist"
 echo OPTIONS_FILE=$OPTIONS_FILE
 
 EXPORT_PATH="$PWD/${DATE_PATH}"
 echo EXPORT_PATH=$EXPORT_PATH
 
-ARCHIVE_PATH="${EXPORT_PATH}/${SCHEME}.xcarchive"
+ARCHIVE_PATH="${EXPORT_PATH}/${SCHEME_NAME}.xcarchive"
 echo ARCHIVE_PATH=$ARCHIVE_PATH
 
-PLIST="$PWD/${PROJECT}/main/${SCHEME}.plist"
+PLIST="$PWD/${PROJECT_NAME}/main/${SCHEME_NAME}.plist"
 echo PLIST=$PLIST
 
 BUILD=$(date +%Y.%m.%d.%H%M%S)
 echo BUILD=$BUILD
+
+mkdir $ARCHIVES_PATH
+mkdir $DATE_PATH
 
 /usr/libexec/PlistBuddy -c "Set CFBundleVersion $BUILD" "${PLIST}"
 
 export LANG=en_US.UTF-8
 pod deintegrate
 pod install  --verbose --repo-update
-#pod install --verbose
 
-mkdir $ARCHIVES_PATH
-mkdir $DATE_PATH
-
-xcodebuild clean -workspace ${WORKSPACE} -scheme ${SCHEME} -configuration Release
-xcodebuild archive -workspace ${WORKSPACE} -scheme ${SCHEME} -archivePath ${ARCHIVE_PATH}
+xcodebuild clean -workspace ${WORKSPACE} -scheme ${SCHEME_NAME} -configuration Release
+xcodebuild archive -workspace ${WORKSPACE} -scheme ${SCHEME_NAME} -archivePath ${ARCHIVE_PATH}
 xcodebuild -exportArchive -exportOptionsPlist ${OPTIONS_FILE} -archivePath ${ARCHIVE_PATH} -exportPath ${EXPORT_PATH}
 
 open ${EXPORT_PATH}
